d00f378a76cc9df5ea39b30684c8a80e84d1029c
Swaroop R <swarooprbca24@rvu.edu.in>
2025-08-26 18:29:28 +0530

    Add files via upload

---
+++ Lab 2/output/Car.java
@@ -0,0 +1,26 @@
+package project1;
+
+public class Car {
+	String brand;
+	int year;
+	
+	// Constructor
+	Car(String b, int y) {
+		brand = b;
+		year = y;
+	}
+	
+	void display() {
+		System.out.println("Car: " + brand + " (" + year + ")");
+	}
+	
+	public static void main(String[] args) {
+		Car c1 = new Car("Toyota", 2020);
+		Car c2 = new Car("Honda", 2022);
+		
+		c1.display();
+		c2.display();
+	}
+}
+
+
