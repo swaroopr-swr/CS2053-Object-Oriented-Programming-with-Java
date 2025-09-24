e628669452f0c6ac9541537c24d18b22b82040f5
Swaroop R <swarooprbca24@rvu.edu.in>
2025-09-24 12:43:11 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,15 @@
+package Project;
+
+public class Example {
+	public static void main(String[] args) {
+		int num;
+		num = 100;
+		
+		System.out.println("This is num: " + num);
+		
+		num = num*2;
+		
+		System.out.println("This is num*2: " + num);
+	}
+
+}
